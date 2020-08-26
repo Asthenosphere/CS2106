ace994ae94880207d6f7b3d5050493364b400c98
Asthenosphere <e0271247@u.nus.edu>
2020-08-26 21:48:50 +0800

    complete ex4

--- lab1/ex4/check_system.sh
+++ lab1/ex4/check_system.sh
@@ -2,18 +2,18 @@
 
 ####################
 # Lab 1 Exercise 4
-# Name:
-# Student No:
-# Lab Group: 
+# Name: Wang Luo
+# Student No: A0180092L
+# Lab Group: 09
 ####################
 
 # fill the below up
-hostname=
-kernel_version=
-process_cnt=
-user_process_cnt=
-mem_usage=
-swap_usage=
+hostname=xcne6
+kernel_version=$(uname -r)
+process_cnt=$(ps -e --no-heading | wc -l)
+user_process_cnt=$(ps -U "$USER" --no-heading | wc -l)
+mem_usage=$(free | grep Mem | awk '{$3/$2 * 100.0}')
+swap_usage=$(free | grep Swap | awk '{$3/$2 * 100.0}')
 
 echo "Hostname: $hostname"
 echo "Linux Kernel Version: $kernel_version"
