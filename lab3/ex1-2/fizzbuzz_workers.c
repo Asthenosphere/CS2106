e7901b534af97ba88435f6e54d934b0fc7333111
Asthenosphere <e0271247@u.nus.edu>
2020-10-10 21:27:54 +0800

    Try to fix ex2

--- lab3/ex1-2/fizzbuzz_workers.c
+++ lab3/ex1-2/fizzbuzz_workers.c
@@ -16,6 +16,7 @@ sem_t *num_sem;
 sem_t *fizz_sem;
 sem_t *buzz_sem;
 sem_t *fizzbuzz_sem;
+sem_t *mutex;
 
 void fizzbuzz_init ( int n ) {
     count = n;
@@ -23,7 +24,8 @@ void fizzbuzz_init ( int n ) {
     fizz_sem = malloc(sizeof(sem_t));
     buzz_sem = malloc(sizeof(sem_t));
     fizzbuzz_sem = malloc(sizeof(sem_t));
-    if (num_sem == NULL || fizz_sem == NULL || buzz_sem == NULL || fizzbuzz_sem == NULL) {
+    mutex = malloc(sizeof(sem_t));
+    if (num_sem == NULL || fizz_sem == NULL || buzz_sem == NULL || fizzbuzz_sem == NULL || mutex == NULL) {
         fprintf(stderr, "Error allocating memory for semaphores.\n");
         exit(1);
     }
@@ -31,13 +33,12 @@ void fizzbuzz_init ( int n ) {
     sem_init(fizz_sem, 0, 0);
     sem_init(buzz_sem, 0, 0);
     sem_init(fizzbuzz_sem, 0, 0);
+    sem_init(mutex, 0, 0);
 }
 
 void num_thread( int n, void (*print_num)(int) ) {
-    for (int i = 1; i <= n + 1; i++) {
-        if (i == n + 1) {
-            return;
-        }
+    for (int i = 1; i <= n; i++) {
+        sem_wait(mutex);
         sem_wait(num_sem);
         if (i % 3 == 0 && i % 5 == 0) {
             sem_post(fizzbuzz_sem);
@@ -49,36 +50,43 @@ void num_thread( int n, void (*print_num)(int) ) {
             print_num(i);
             sem_post(num_sem);
         }
+        sem_post(mutex);
     }
 }
 
 void fizz_thread( int n, void (*print_fizz)(void) ) {
     for (int i = 1; i <= n; i++) {
+        sem_wait(mutex);
         if (i % 3 == 0) {
             sem_wait(fizz_sem);
             print_fizz();
             sem_post(num_sem);
         }
+        sem_post(mutex);
     }
 }
 
 void buzz_thread( int n, void (*print_buzz)(void) ) {
     for (int i = 1; i <= n; i++) {
+        sem_wait(mutex);
         if (i % 5 == 0) {
             sem_wait(buzz_sem);
             print_buzz();
             sem_post(num_sem);
         }
+        sem_post(mutex);
     }
 }
 
 void fizzbuzz_thread( int n, void (*print_fizzbuzz)(void) ) {
     for (int i = 1; i <= n; i++) {
+        sem_wait(mutex);
         if (i % 3 == 0 && i % 5 == 0) {
             sem_wait(fizzbuzz_sem);
             print_fizzbuzz();
             sem_post(num_sem);
         }
+        sem_post(mutex);
     }
 }
 
