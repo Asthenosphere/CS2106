bf3f2e31793707222cd88c16c1943202846e1bbf
Asthenosphere <e0271247@u.nus.edu>
2020-10-12 21:03:22 +0800

    Add print

--- lab3/ex5/exit_controller.c
+++ lab3/ex5/exit_controller.c
@@ -70,13 +70,17 @@ void exit_controller_post(exit_controller_t *exit_controller, int priority) {
     sem_wait(exit_controller->mutex);
     exit_controller->line_empty = 1;
     int next_highest_priority_index = -1;
+    for (int i = 0; i < 50; i++) {
+        printf("%d ", exit_controller->frequencies[i]);
+    }
+    printf("\n");
     for (int i = 0; i < priority; i++) {
         if (exit_controller->frequencies[i] > 0) {
             next_highest_priority_index = i;
             break;
         }
     }
-    if (next_highest_priority_index != priority) {
+    if (next_highest_priority_index != -1) {
         exit_controller->frequencies[next_highest_priority_index]--;
         sem_post(exit_controller->locks[next_highest_priority_index]);
     } else {
