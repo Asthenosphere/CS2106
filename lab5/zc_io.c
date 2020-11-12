df6953eaa4bd877c7d6caa2882f3df8af5668b90
Asthenosphere <e0271247@u.nus.edu>
2020-11-12 18:30:26 +0800

    Initial commit for lab 5

---
+++ lab5/zc_io.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include "zc_io.h"
+
+// The zc_file struct is analogous to the FILE struct that you get from fopen.
+struct zc_file {
+  // Insert the fields you need here.
+  char * ptr;
+  size_t size;
+  size_t offset;
+  int fd;
+};
+
+/**************
+ * Exercise 1 *
+ **************/
+
+zc_file *zc_open(const char *path) {
+  struct zc_file * file = malloc(sizeof(struct zc_file));
+  file->fd = open(path, O_RDWR | O_CREAT);
+  if (file->fd != 0) {
+      fprintf(stderr, "Error opening file");
+      exit(1);
+  }
+  struct stat st;
+  fstat(file->fd, &st);
+  char * addr = mmap(NULL, st.st_size, PROT_WRITE | PROT_READ, MAP_PRIVATE, file->fd, 0);
+  file->ptr = addr;
+  file->size = st.st_size;
+  file->offset = 0;
+  return file;
+}
+
+int zc_close(zc_file *file) {
+  int flag = munmap(file->ptr, file->size);
+  if (flag != 0) {
+      return flag;
+  }
+  free(file);
+  return 0;
+}
+
+const char *zc_read_start(zc_file *file, size_t *size) {
+  char *res = file->ptr;
+  res += *size;
+  file->offset += *size;
+  return res;
+}
+
+void zc_read_end(zc_file *file) {
+  // To implement
+}
+
+/**************
+ * Exercise 2 *
+ **************/
+
+char *zc_write_start(zc_file *file, size_t size) {
+  // To implement
+  return NULL;
+}
+
+void zc_write_end(zc_file *file) {
+  // To implement
+}
+
+/**************
+ * Exercise 3 *
+ **************/
+
+off_t zc_lseek(zc_file *file, long offset, int whence) {
+  // To implement
+  return -1;
+}
+
+/**************
+ * Exercise 5 *
+ **************/
+
+int zc_copyfile(const char *source, const char *dest) {
+  // To implement
+  return -1;
+}
